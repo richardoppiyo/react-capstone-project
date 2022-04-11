22073c39c02d873e9e100b18eeafb34b15625e8b
Richard Opiyo <romenyo@dewcis.com>
2022-04-11 20:54:39 +0300

    fix linters

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import myReducer from './apis/weather';
+
+const rootReducer = combineReducers({
+  myred: myReducer,
 });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
