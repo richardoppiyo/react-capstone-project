3fc5b60f2bef6f64269784acb785fd48157b6e36
Richard Opiyo <romenyo@dewcis.com>
2022-04-12 21:08:33 +0300

    details page layout

--- src/features/details.js
+++ src/features/details.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const Details = () => (
   <>
+    <div>
+      <p>Total Cases: 10000</p>
+      <p>Total tested: 10000</p>
+      <p>Population: 10000</p>
+      <p>Total deaths: 10000</p>
+      <p>Total recovered: 10000</p>
+      <p>Continent: 10000</p>
+    </div>
     <h1>Details page</h1>
     <a href="/">To Home</a>
   </>
